9a87d98260556ffc77bd2d01dc99caab0e4cc0f8
root <macunan@gmail.com>
2023-02-26 21:20:10 -0300

    Added ffmpeg support to allow aac radios

--- fmtrans/backupffmpeg_pyfunctions.py
+++ fmtrans/backupffmpeg_pyfunctions.py
@@ -8,7 +8,6 @@ class Server_Ops:
     def restart(self):
        os.system("systemctl restart iradio")
     def writefile(self,id):
-       os.system("pkill ffmpeg")
        id=int(id)
        config = Config.objects.latest('id')
        url=Radios.objects.get(id=id).url
@@ -18,15 +17,12 @@ class Server_Ops:
        if len(radio_name)>64:
         radio_name=radio_name[0:64]
 
-       end=" -f wav -bitexact -acodec pcm_s16le -ar 22050 -ac 2 -y /files/movies/movies/radio.wav &" 
-       end2=" sleep 2"+ "\n"+" fm_transmitter -f "+freq+" -d 3 -b 300  /files/movies/movies/radio.wav"
-
-# start="curl "+url+"|egrep -o 'https?:.*'|tail -n 1"
-# os.system(start+" > "+home_loc+"radio.sh")
-# f = open(home_loc+"radio.sh", "r")
-# initial=f.read()
-# f.close()
-# initial=initial.rstrip()
+       end=" -ar 44100  -f wav - |fm_transmitter -f "+freq+" -d 3 -"
+       start="curl "+url+"|egrep -o 'http.*'|tail -n 1" 
+       os.system(start+" > "+home_loc+"radio.sh")
+       f = open(home_loc+"radio.sh", "r")
+       initial=f.read() 
+       f.close()
 # end=" -t wav - |pi_fm_adv --freq "+freq+ " --rt '"+radio_name+"' --audio -"
 # end="  -r 44100 -b 16 -e signed -t wav - |fm_transmitter -f  91.5 -"
 # end=" -r 22050 -c 1 -b 16 -t wav - |fm_transmitter -f "+freq+" -d 3 -"
@@ -36,11 +32,10 @@ class Server_Ops:
 
        init="#!/bin/bash"
 # script=start+url+end
-       script="nohup ffmpeg -i "+url+end
+       script="ffmpeg -i "+initial+end
        file = open(home_loc+"radio.sh", "w")
        file.write(init+"\n")
        file.write(script)
-       file.write(end2)
        file.close()
     def run_fast_scandir(self,dir):    # dir: str, ext: list
         s="";
